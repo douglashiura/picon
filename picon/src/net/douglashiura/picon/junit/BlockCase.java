76069ef0bf383107012757d2b69e7cf690219c73
douglashiura <douglashiura@gmail.com>
2018-03-18 22:27:57 -0300

    Add to commit

---
+++ picon/src/net/douglashiura/picon/junit/BlockCase.java
@@ -0,0 +1,37 @@
+package net.douglashiura.picon.junit;
+
+public class BlockCase {
+	static String camelCase(String name) {
+		String camelCase = "";
+		boolean sinalNumerico = false;
+		boolean sinalUpper = false;
+
+		for (Character n : name.toCharArray()) {
+			if (n == '_') {
+				camelCase += n;
+			} else if (Character.isUpperCase(n) && !sinalUpper) {
+				camelCase += ":" + n;
+				sinalUpper = true;
+				sinalNumerico = false;
+			} else if (Character.isDigit(n) && !sinalNumerico) {
+				camelCase += ":" + n;
+				sinalUpper = false;
+				sinalNumerico = true;
+			} else if (Character.isUpperCase(n) && sinalUpper) {
+				camelCase += n;
+				sinalNumerico = false;
+			} else if (Character.isDigit(n) && sinalNumerico) {
+				camelCase += n;
+				sinalUpper = false;
+			} else {
+				camelCase += n;
+				if ((!Character.isUpperCase(n) || !Character.isDigit(n))) {
+					sinalNumerico = false;
+					sinalUpper = false;
+				}
+			}
+		}
+		return camelCase.toLowerCase();
+
+	}
+}
