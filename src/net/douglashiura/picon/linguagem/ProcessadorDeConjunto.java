320a20efe9a0f151d1e874ba0b1ed00ce198b3d9
douglas.hiura <douglas.hiura@DESEN002.upcc.srv>
2019-08-30 17:04:57 -0300

    class loader adjust

--- src/net/douglashiura/picon/linguagem/ProcessadorDeConjunto.java
+++ src/net/douglashiura/picon/linguagem/ProcessadorDeConjunto.java
@@ -1,10 +1,12 @@
 package net.douglashiura.picon.linguagem;
 
+import java.net.URLClassLoader;
 import java.util.Deque;
 
 import net.douglashiura.picon.ProblemaDeCompilacaoException;
 import net.douglashiura.picon.linguagem.atribuicao.ProcessadorDeCampos;
 import net.douglashiura.picon.preguicoso.Objeto;
+import sun.misc.Launcher;
 
 public class ProcessadorDeConjunto {
 
@@ -17,7 +19,7 @@ public class ProcessadorDeConjunto {
 	public void processar(Deque<Parte> emClasse) throws ProblemaDeCompilacaoException {
 		Parte parteClasse = emClasse.pop();
 		try {
-			Class<?> klasse = Class.forName(parteClasse.valor());
+			Class<?> klasse = buscarClasse(parteClasse.valor());
 			ProcessadorDeCampos processador = new ProcessadorDeCampos(qualificadores);
 			emClasse.pop();
 			while (!"}".equals(emClasse.peek().valor())) {
@@ -30,7 +32,25 @@ public class ProcessadorDeConjunto {
 		} catch (ClassNotFoundException e) {
 			throw new ProblemaDeCompilacaoException(e, parteClasse);
 		}
-
+	}
+	
+	
+	private Class<?> buscarClasse(String parteClasse) throws ClassNotFoundException {
+		ClassLoader[] carregadores = {  ClassLoader.getSystemClassLoader(), Thread.currentThread().getContextClassLoader().getParent(), 
+				URLClassLoader.getSystemClassLoader(), Launcher.getLauncher().getClassLoader(), Launcher.getLauncher().getClassLoader().getParent() };
+		for (ClassLoader carregador : carregadores) {
+			System.out.println(carregador.getClass()+" class:"+ parteClasse);
+			try {
+				return carregar(parteClasse, carregador);
+			} catch (ClassNotFoundException semClasse) {
+			}
+		}
+		throw new ClassNotFoundException();
 	}
 
+	private Class<?> carregar(String nome, ClassLoader load) throws ClassNotFoundException {
+		return load.loadClass(nome);
+	}
+	
+	
 }
